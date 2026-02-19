0b7014ee5a9f8ec7972fcabdddaab997d5e4768c
osilver17 <osilver@internet.ru>
2026-02-19 17:58:10 +0300

    comments export/import

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 "use strict";
-
-// Хранилище данных приложения
-const comments = [
-    {
-        dataName: "Глеб Фокин",
-        dataDateTime: "12.02.22 12:18",
-        dataComment: "Это будет первый комментарий на этой странице",
-        dataIsLiked: false,
-        dataLikesCounter: 3,
-    },
-    {
-        dataName: "Варвара Н.",
-        dataDateTime: "13.02.22 19:22",
-        dataComment: "Мне нравится как оформлена эта страница! ❤",
-        dataIsLiked: true,
-        dataLikesCounter: 75,
-    },
-];
-
+import {comments} from './modules/appData.js';
 // Опции для преобразования дат и времени в комментариях
 const dateOptions = {
     day: 'numeric',
---
+++ modules/appData.js
@@ -0,0 +1,17 @@
+// Хранилище данных приложения
+export const comments = [
+    {
+        dataName: "Глеб Фокин",
+        dataDateTime: "12.02.22 12:18",
+        dataComment: "Это будет первый комментарий на этой странице",
+        dataIsLiked: false,
+        dataLikesCounter: 3,
+    },
+    {
+        dataName: "Варвара Н.",
+        dataDateTime: "13.02.22 19:22",
+        dataComment: "Мне нравится как оформлена эта страница! ❤",
+        dataIsLiked: true,
+        dataLikesCounter: 75,
+    },
+];
