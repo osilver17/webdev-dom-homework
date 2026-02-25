e081c84550a489d5f86956203a8a06835a5b92f1
osilver17 <osilver@internet.ru>
2026-02-25 15:22:48 +0300

    API 3. Обработка ошибок

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 "use strict";
-import { commentsRenderer } from './modules/view.js';
+
 import { addComment } from './modules/listeners.js';
-import { renewComments, comments } from './modules/model.js';
+import { getComments } from './modules/api.js';
 
-
-// Функция запроса данных с сервера и обновления локальных данных в модели
-fetch('https://wedev-api.sky.pro/api/v1/oleg-serebrennikov/comments', {
-    method: "GET",
-})
-    .then((response) => response.json())
-    .then((data) => {
-        renewComments(data.comments);
-    })
-    .then(() => {
-        // Отрисовка данных из хранилища
-        console.log('рендерим массив из локального хранилища при начальной загрузке...');
-        commentsRenderer(comments);
-    })
-    .catch((error) => console.log(error));
+// Вызов функции получения всех комментариев
+getComments();
 
 // Вызов функции добавления нового комментария 
 addComment();
